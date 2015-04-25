e2be6bdebef9b2d2a7de53c5086ad56c3b0baff4
Christopher Nikkel <chris@immortalcreation.com>
2015-04-25 10:17:52 -0700

    Made initial iteration and capture work

--- Biff/Program.fs
+++ Biff/Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open System
 open System.Windows.Forms
 open System.Windows.Forms.DataVisualization.Charting
 
@@ -21,11 +22,33 @@ type LineChartForm(title, xs : float seq) =
 
 [<EntryPoint>]
 let main argv = 
-    let f_biff x r = r * (1.0 - 2.0 * x) 
     let f_sine x = sin(x / 100.0)
-    let data = seq { for i in 1..1000 do yield f_sine (float i) }
-    let form = new LineChartForm("Sine", data)
-    System.Windows.Forms.Application.Run(form)
 
-    printfn "%A" argv
+    let f_biff x = 2.4 * (1.0 - 2.0 * x)
+
+    let rec iterate x i = 
+        if i > 1 then
+            let y = f_biff x
+            let result = iterate y (i - 1)
+            result
+        else
+            f_biff x
+
+    printfn "%A" (iterate 2.1 2)
+
+    let rec iterateCapture x i = 
+        if i > 1 then
+            let y = f_biff x
+            let result = iterateCapture y (i - 1)
+            y :: result
+        else
+            []
+
+    printfn "%A" (iterateCapture (iterate 2.1 2) 10)
+
+    let data = seq { for i in 1..1000 -> f_sine (float i) }
+
+    let form = new LineChartForm("Sine", data)
+    printfn "%A" data2
+    System.Windows.Forms.Application.Run(form)
     0 // return an integer exit code
