1602f740db77b36e8ca363e702e0f5ab1973114d
Christopher Nikkel <chris@immortalcreation.com>
2015-09-04 18:02:23 -0700

    Prep for getting timer added

--- Biff/Program.fs
+++ Biff/Program.fs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c)  2015 Christopher Nikkel
-
 open FSharp.Charting
 open System
 open System.Drawing
-open FSharp.Collections.ParallelSeq
+open System.Windows.Forms
 
 [<EntryPoint>]
 let main argv = 
@@ -25,6 +24,8 @@ let main argv =
             fx :: result
         else
             []
+    let calcBiff x rate iterations samples f_biff = 
+        iterateCapture (iterate x rate iterations f_biff) rate samples f_biff) |> List.map (fun fx -> (rate, fx))
 
     let x = 0.5
     let iterations = 2000
@@ -32,14 +33,21 @@ let main argv =
 
     let data = 
         seq {2.5 .. 0.001 .. 4.0 } 
-        |> PSeq.map (fun rate -> 
-            (iterateCapture (iterate x rate iterations f_biff) rate samples f_biff) 
-            |> List.map (fun fx -> (rate, fx))) 
+        |> Seq.map (fun rate -> calcBiff x rate iterations samples f_biff)
         |> List.concat
 
-    let chart = data |> Chart.FastPoint
+    let createTimer interval =
+        let timer = new System.Windows.Forms.Timer(Interval = int(interval * 1000.0), Enabled = true)
+        timer.Start()
+        timer.Tick
+
+    //let events = createTimer 0.1
+    //let eventStream = events |> Observable.map (fun data _ -> doIteration data) initialData
+    //let chart = LiveChart.FastPoint(eventStream, Name = "Biff").WithXAxis(Enabled = false).WithYAxis(Enabled = false)
+    let chart = Chart.FastPoint(data, Name = "Biff").WithXAxis(Enabled = false).WithYAxis(Enabled = false)
     let form = chart.ShowChart()
+    form.Width <- 1000
+    form.Height <- 1000
 
     System.Windows.Forms.Application.Run(form)
-
     0 // return an integer exit code
